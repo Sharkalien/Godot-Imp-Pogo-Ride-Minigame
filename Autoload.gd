3fc2d0b0b17e8906e307fa10abed3ca5dad9df5a
Leo Esc <leonardodanielescoto@gmail.com>
2022-06-26 23:41:32 -0500

    Add scoreboard stuff

--- Autoload.gd
+++ Autoload.gd
@@ -6,3 +6,16 @@ var volumeClicks : int
 
 #func _process(_delta):
 #	print(str(combos) + " autoload")
+
+func _ready():
+	var f = File.new()
+	f.open('res://apiKey.env',File.READ)
+	var apiKey = f.get_line()
+	f.close()
+	
+	SilentWolf.configure({
+	"api_key": apiKey,
+	"game_id": "ImpPogoRide",
+	"game_version": "1.0",
+	"log_level": 1
+})
