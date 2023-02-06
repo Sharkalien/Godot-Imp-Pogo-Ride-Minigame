f1f95e9d9bdf54350dff4c85af96a460eade5c93
Leo Esc <leonardodanielescoto@gmail.com>
2023-02-06 16:15:26 -0600

    Finish custom HTML shell export template
    Also sorted reference images into their own folder so they can be excluded from the export.
    
    Also also ran into a bug where input on mobile wasn't being received because I was only getting input from Device 0 rather than all devices.
    
    Also also also x3 combo tapping on the controls button on mobile caused it to immediately close, so I changed the action mode to Button Release

--- ui/Dialog Box/Dialog_Player.gd
+++ ui/Dialog Box/Dialog_Player.gd
@@ -5,7 +5,7 @@ onready var rootNode = get_node(".")
 
 
 func _on_DialogBox_gui_input(_event):
-	if Input.is_action_pressed("click"):
+	if Input.is_action_pressed("click") and rootNode.visible:
 		animPlayer.play_backwards("Open")
 		yield(get_tree().create_timer(0.17), "timeout")
 		rootNode.visible = false
