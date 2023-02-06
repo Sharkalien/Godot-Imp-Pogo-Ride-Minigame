4b5315c936a638aabd28391d17a084f6b38784ad
Leo Esc <leonardodanielescoto@gmail.com>
2023-02-05 19:46:10 -0600

    Add back in MSPFA username JS code
    Even though it doesn't work and is commented out

--- Autoload.gd
+++ Autoload.gd
@@ -16,3 +16,19 @@ func _ready():
 	"game_version": "1.0",
 	"log_level": 1
 	})
+	
+	# Get username from MSPFA if the player is logged in (currently doesn't work :/)
+#	JavaScript.eval("""
+#	let playerName;
+#	window.addEventListener("message", (event) => {
+#		if (event.origin !== "https://mspfa.com") return;
+#
+#		playerName = event.data
+#		console.log('Player name is: ' + playerName)
+#		}, false);
+#	""", true)
+#	if JavaScript.eval("typeof playerName !== 'undefined'", true):
+#		Autoload.player = JavaScript.eval("playerName", true)
+
+#func _process(_delta):
+#	print(str(combos) + " autoload")
